00b05cbe0303f873bbc3a528ca9428ec6b80df79
rumikmeow-byte <rumik.meow@gmail.com>
2026-08-30 01:00:09 +0500

    Update config.py

--- config.py
+++ config.py
@@ -3,21 +3,19 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-# === ОБЯЗАТЕЛЬНО ЗАПОЛНИ ===
-BOT_TOKEN = os.getenv("BOT_TOKEN", "ВАШ_ТОКЕН_ОТ_BOTFATHER")
-BOT_USERNAME = os.getenv("BOT_USERNAME", "YourBotUsername")  # без @
+# === НАСТРОЙКИ ===
 
-# Канал, на который нужно подписаться (username без @ или id вида -100...)
-CHANNEL_ID = os.getenv("CHANNEL_ID", "@your_channel")  # например @Eclipsed_channel
-CHANNEL_LINK = os.getenv("CHANNEL_LINK", "https://t.me/your_channel")
+BOT_TOKEN = "ВСТАВЬ_НОВЫЙ_ТОКЕН_ОТ_BOTFATHER"
 
-# Поддержка
+BOT_USERNAME = "GiftsEz_bot"
+
+CHANNEL_ID = "@Eclipsedlf"
+CHANNEL_LINK = "https://t.me/Eclipsedlf"
+
 SUPPORT_USERNAME = "Eclipsed_consult"
 SUPPORT_LINK = f"https://t.me/{SUPPORT_USERNAME}"
 
-# Куда отправлять заявки на вывод (ID админа или чата). 
-# Получить свой ID: напиши боту @userinfobot
-ADMIN_ID = int(os.getenv("ADMIN_ID", "0"))  # замени на свой Telegram ID
+ADMIN_ID = 8644223884
 
 # Реферальный бонус
 REFERRAL_BONUS = 0.85
